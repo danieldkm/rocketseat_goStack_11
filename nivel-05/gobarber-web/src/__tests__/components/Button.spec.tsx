d3deab4a1914d215301b891ceb15ba7ac556b0be
danielmorita <danielkeyti@gmail.com>
2020-07-02 18:20:08 -0300

    Finalizando testes no login

---
+++ nivel-05/gobarber-web/src/__tests__/components/Button.spec.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { render, fireEvent, wait } from '@testing-library/react';
+import Button from '../../components/Button';
+
+jest.mock('@unform/core', () => {
+  return {
+    useField() {
+      return {
+        fieldName: 'email',
+        defaultValue: '',
+        error: '',
+        registerField: jest.fn(),
+      };
+    },
+  };
+});
+
+describe('Button component', () => {
+  it('should be able to render an button', () => {
+    const { getByPlaceholderText } = render(
+      <Button type="button" placeholder="Button" />,
+    );
+
+    expect(getByPlaceholderText('Button')).toBeTruthy();
+  });
+  it('should be able to render an button with loading', () => {
+    const { getByPlaceholderText } = render(
+      <Button type="button" placeholder="Button" loading />,
+    );
+
+    expect(getByPlaceholderText('Button')).toBeTruthy();
+  });
+});
